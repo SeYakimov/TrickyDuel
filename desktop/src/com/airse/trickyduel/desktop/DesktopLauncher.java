8e82e32e1af48efe89b602aa3aed9a0f9a978b36
SeYakimov <qwertyqwerty407@gmail.com>
2017-06-17 12:12:23 +0300

    Initial commit

---
+++ desktop/src/com/airse/trickyduel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.airse.trickyduel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.airse.trickyduel.Duel;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Duel(), config);
+	}
+}
---
+++ html/src/com/airse/trickyduel/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.airse.trickyduel.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.airse.trickyduel.Duel;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Duel();
+        }
+}
